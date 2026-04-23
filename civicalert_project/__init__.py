c159f9d43ce684133bc321659b63e185077db750
User <user@example.com>
2026-04-23 20:20:12 +0545

    Remove pymysql import from __init__.py for PostgreSQL compatibility

--- civicalert_project/__init__.py
+++ civicalert_project/__init__.py
@@ -1,5 +1,1 @@
-import pymysql
-
-# This tricks Django into thinking mysqlclient is installed and up to date
-pymysql.version_info = (2, 2, 7, "final", 0) 
-pymysql.install_as_MySQLdb()
+# Removed pymysql import as we're using PostgreSQL now
